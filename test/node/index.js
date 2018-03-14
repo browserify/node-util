20e34771e9803570854ed5c8caac52fb1840f261
Renée Kooi <renee@kooi.me>
2018-03-14 09:21:48 +0100

    fix running node tests on 0.10

--- test/node/index.js
+++ test/node/index.js
@@ -1,6 +1,19 @@
-var spawn = require('child_process').spawnSync;
+var spawn = require('child_process').spawn;
+var path = require('path');
+var series = require('run-series');
 
-spawn(process.argv[0], [ require.resolve('./debug') ], { stdio: 'inherit' });
-spawn(process.argv[0], [ require.resolve('./format') ], { stdio: 'inherit' });
-spawn(process.argv[0], [ require.resolve('./inspect') ], { stdio: 'inherit' });
-spawn(process.argv[0], [ require.resolve('./log') ], { stdio: 'inherit' });
+function test(filename) {
+  return function(cb) {
+    var proc = spawn(process.argv[0], [ filename ], { stdio: 'inherit' });
+    proc.on('close', function (code) {
+      cb(code !== 0 ? new Error('test ' + path.basename(filename) + ' failed') : null);
+    });
+  };
+}
+
+series([
+  test(require.resolve('./debug')),
+  test(require.resolve('./format')),
+  test(require.resolve('./inspect')),
+  test(require.resolve('./log'))
+]);
