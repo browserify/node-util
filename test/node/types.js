1efe000feef9a61f77b356ee2815d657bf078a18
Luke Childs <lukechilds123@gmail.com>
2019-04-24 18:46:37 +0700

    Only run typed array value check tests against supported types

--- test/node/types.js
+++ test/node/types.js
@@ -134,70 +134,110 @@ console.log('Testing', 'isBoxedPrimitive');
 });
 
 {
-  const primitive = true;
-  const arrayBuffer = new ArrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  const dataView = new DataView(arrayBuffer);
-  const uint8Array = new Uint8Array(arrayBuffer);
-  const uint8ClampedArray = new Uint8ClampedArray(arrayBuffer);
-  const uint16Array = new Uint16Array(arrayBuffer);
-  const uint32Array = new Uint32Array(arrayBuffer);
-  const int8Array = new Int8Array(arrayBuffer);
-  const int16Array = new Int16Array(arrayBuffer);
-  const int32Array = new Int32Array(arrayBuffer);
-  const float32Array = new Float32Array(arrayBuffer);
-  const float64Array = new Float64Array(arrayBuffer);
-  const bigInt64Array = new BigInt64Array(arrayBuffer);
-  const bigUint64Array = new BigUint64Array(arrayBuffer);
+  let primitive = () => true;
+  let arrayBuffer = () => new ArrayBuffer();
 
-  const fakeBuffer = Object.create(Buffer.prototype);
-  const fakeDataView = Object.create(DataView.prototype);
-  const fakeUint8Array = Object.create(Uint8Array.prototype);
-  const fakeUint8ClampedArray = Object.create(Uint8ClampedArray.prototype);
-  const fakeUint16Array = Object.create(Uint16Array.prototype);
-  const fakeUint32Array = Object.create(Uint32Array.prototype);
-  const fakeInt8Array = Object.create(Int8Array.prototype);
-  const fakeInt16Array = Object.create(Int16Array.prototype);
-  const fakeInt32Array = Object.create(Int32Array.prototype);
-  const fakeFloat32Array = Object.create(Float32Array.prototype);
-  const fakeFloat64Array = Object.create(Float64Array.prototype);
-  const fakeBigInt64Array = Object.create(BigInt64Array.prototype);
-  const fakeBigUint64Array = Object.create(BigUint64Array.prototype);
+  let buffer = () => Buffer.from(arrayBuffer());
+  let dataView = () => new DataView(arrayBuffer());
+  let uint8Array = () => new Uint8Array(arrayBuffer());
+  let uint8ClampedArray = () => new Uint8ClampedArray(arrayBuffer());
+  let uint16Array = () => new Uint16Array(arrayBuffer());
+  let uint32Array = () => new Uint32Array(arrayBuffer());
+  let int8Array = () => new Int8Array(arrayBuffer());
+  let int16Array = () => new Int16Array(arrayBuffer());
+  let int32Array = () => new Int32Array(arrayBuffer());
+  let float32Array = () => new Float32Array(arrayBuffer());
+  let float64Array = () => new Float64Array(arrayBuffer());
+  let bigInt64Array = () => new BigInt64Array(arrayBuffer());
+  let bigUint64Array = () => new BigUint64Array(arrayBuffer());
 
-  const stealthyDataView =
-    Object.setPrototypeOf(new DataView(arrayBuffer), Uint8Array.prototype);
-  const stealthyUint8Array =
-    Object.setPrototypeOf(new Uint8Array(arrayBuffer), ArrayBuffer.prototype);
-  const stealthyUint8ClampedArray =
+  let fakeBuffer = () => Object.create(Buffer.prototype);
+  let fakeDataView = () => Object.create(DataView.prototype);
+  let fakeUint8Array = () => Object.create(Uint8Array.prototype);
+  let fakeUint8ClampedArray = () => Object.create(Uint8ClampedArray.prototype);
+  let fakeUint16Array = () => Object.create(Uint16Array.prototype);
+  let fakeUint32Array = () => Object.create(Uint32Array.prototype);
+  let fakeInt8Array = () => Object.create(Int8Array.prototype);
+  let fakeInt16Array = () => Object.create(Int16Array.prototype);
+  let fakeInt32Array = () => Object.create(Int32Array.prototype);
+  let fakeFloat32Array = () => Object.create(Float32Array.prototype);
+  let fakeFloat64Array = () => Object.create(Float64Array.prototype);
+  let fakeBigInt64Array = () => Object.create(BigInt64Array.prototype);
+  let fakeBigUint64Array = () => Object.create(BigUint64Array.prototype);
+
+  let stealthyDataView = () =>
+    Object.setPrototypeOf(new DataView(arrayBuffer()), Uint8Array.prototype);
+  let stealthyUint8Array = () =>
+    Object.setPrototypeOf(new Uint8Array(arrayBuffer()), ArrayBuffer.prototype);
+  let stealthyUint8ClampedArray = () =>
     Object.setPrototypeOf(
-      new Uint8ClampedArray(arrayBuffer), ArrayBuffer.prototype
+      new Uint8ClampedArray(arrayBuffer()), ArrayBuffer.prototype
     );
-  const stealthyUint16Array =
-    Object.setPrototypeOf(new Uint16Array(arrayBuffer), Uint16Array.prototype);
-  const stealthyUint32Array =
-    Object.setPrototypeOf(new Uint32Array(arrayBuffer), Uint32Array.prototype);
-  const stealthyInt8Array =
-    Object.setPrototypeOf(new Int8Array(arrayBuffer), Int8Array.prototype);
-  const stealthyInt16Array =
-    Object.setPrototypeOf(new Int16Array(arrayBuffer), Int16Array.prototype);
-  const stealthyInt32Array =
-    Object.setPrototypeOf(new Int32Array(arrayBuffer), Int32Array.prototype);
-  const stealthyFloat32Array =
+  let stealthyUint16Array = () =>
+    Object.setPrototypeOf(new Uint16Array(arrayBuffer()), Uint16Array.prototype);
+  let stealthyUint32Array = () =>
+    Object.setPrototypeOf(new Uint32Array(arrayBuffer()), Uint32Array.prototype);
+  let stealthyInt8Array = () =>
+    Object.setPrototypeOf(new Int8Array(arrayBuffer()), Int8Array.prototype);
+  let stealthyInt16Array = () =>
+    Object.setPrototypeOf(new Int16Array(arrayBuffer()), Int16Array.prototype);
+  let stealthyInt32Array = () =>
+    Object.setPrototypeOf(new Int32Array(arrayBuffer()), Int32Array.prototype);
+  let stealthyFloat32Array = () =>
     Object.setPrototypeOf(
-      new Float32Array(arrayBuffer), Float32Array.prototype
+      new Float32Array(arrayBuffer()), Float32Array.prototype
     );
-  const stealthyFloat64Array =
+  let stealthyFloat64Array = () =>
     Object.setPrototypeOf(
-      new Float64Array(arrayBuffer), Float64Array.prototype
+      new Float64Array(arrayBuffer()), Float64Array.prototype
     );
-  const stealthyBigInt64Array =
+  let stealthyBigInt64Array = () =>
     Object.setPrototypeOf(
-      new BigInt64Array(arrayBuffer), BigInt64Array.prototype
+      new BigInt64Array(arrayBuffer()), BigInt64Array.prototype
     );
-  const stealthyBigUint64Array =
+  let stealthyBigUint64Array = () =>
     Object.setPrototypeOf(
-      new BigUint64Array(arrayBuffer), BigUint64Array.prototype
+      new BigUint64Array(arrayBuffer()), BigUint64Array.prototype
     );
+
+  const typedArrays = Object.entries({
+    primitive, arrayBuffer, buffer, fakeBuffer,
+    dataView, fakeDataView, stealthyDataView,
+    uint8Array, fakeUint8Array, stealthyUint8Array,
+    uint8ClampedArray, fakeUint8ClampedArray, stealthyUint8ClampedArray,
+    uint16Array, fakeUint16Array, stealthyUint16Array,
+    uint32Array, fakeUint32Array, stealthyUint32Array,
+    int8Array, fakeInt8Array, stealthyInt8Array,
+    int16Array, fakeInt16Array, stealthyInt16Array,
+    int32Array, fakeInt32Array, stealthyInt32Array,
+    float32Array, fakeFloat32Array, stealthyFloat32Array,
+    float64Array, fakeFloat64Array, stealthyFloat64Array,
+    bigInt64Array, fakeBigInt64Array, stealthyBigInt64Array,
+    bigUint64Array, fakeBigUint64Array, stealthyBigUint64Array
+  });
+  typedArrays.forEach(([key, createTypedArray]) => {
+    try {
+      typedArrays[key] = createTypedArray();
+    } catch (e) {
+      typedArrays[key] = undefined;
+    }
+  });
+
+  ({
+    primitive, arrayBuffer, buffer, fakeBuffer,
+    dataView, fakeDataView, stealthyDataView,
+    uint8Array, fakeUint8Array, stealthyUint8Array,
+    uint8ClampedArray, fakeUint8ClampedArray, stealthyUint8ClampedArray,
+    uint16Array, fakeUint16Array, stealthyUint16Array,
+    uint32Array, fakeUint32Array, stealthyUint32Array,
+    int8Array, fakeInt8Array, stealthyInt8Array,
+    int16Array, fakeInt16Array, stealthyInt16Array,
+    int32Array, fakeInt32Array, stealthyInt32Array,
+    float32Array, fakeFloat32Array, stealthyFloat32Array,
+    float64Array, fakeFloat64Array, stealthyFloat64Array,
+    bigInt64Array, fakeBigInt64Array, stealthyBigInt64Array,
+    bigUint64Array, fakeBigUint64Array, stealthyBigUint64Array
+  } = typedArrays);
 
   const all = [
     primitive, arrayBuffer, buffer, fakeBuffer,
@@ -281,15 +321,15 @@ console.log('Testing', 'isBoxedPrimitive');
   };
 
   for (const testedFunc of Object.keys(expected)) {
+    console.log('Testing', testedFunc);
     const func = types[testedFunc];
     const yup = [];
-    for (const value of all) {
+    for (const value of all.filter(a => typeof a !== 'undefined')) {
       if (func(value)) {
         yup.push(value);
       }
     }
-    console.log('Testing', testedFunc);
-    assert.deepStrictEqual(yup, expected[testedFunc]);
+    assert.deepStrictEqual(yup, expected[testedFunc].filter(a => typeof a !== 'undefined'));
   }
 }
 
