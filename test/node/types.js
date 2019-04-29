c37c2e3641c2a68619c61269e0c38ee06585100e
Luke Childs <lukechilds123@gmail.com>
2019-04-29 11:15:57 +0700

    Refactor transpiled code to be simpler and not depend on Symbol.iterator

--- test/node/types.js
+++ test/node/types.js
@@ -471,45 +471,24 @@ if (SymbolToStringTagSupported) {
     ]
   };
 
-  for (var _i2 = 0, _Object$keys = Object.keys(expected); _i2 < _Object$keys.length; _i2++) {
-    var testedFunc = _Object$keys[_i2];
-
+  Object.keys(expected).forEach(function (testedFunc) {
     console.log('Testing values for:', testedFunc);
     var func = types[testedFunc];
     var yup = [];
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
 
-    try {
-      for (var _iterator = all.filter(function (a) {
-        return typeof a !== 'undefined';
-      })[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-        var value = _step.value;
-
+    all
+      .filter(function (a) { return typeof a !== 'undefined'; })
+      .forEach(function (value) {
         if (func(value)) {
           yup.push(value);
         }
-      }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
-      try {
-        if (!_iteratorNormalCompletion && _iterator.return != null) {
-          _iterator.return();
-        }
-      } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
-        }
-      }
-    }
+      });
 
-    assert.deepStrictEqual(yup, expected[testedFunc].filter(function (a) {
-      return typeof a !== 'undefined';
-    }));
-  }
+    assert.deepStrictEqual(
+      yup,
+      expected[testedFunc].filter(function (a) { return typeof a !== 'undefined'; })
+    );
+  });
 }
 
 // (async () => {
