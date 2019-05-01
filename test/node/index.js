df78840607eabab3a3784bcda6a841b7e9257ca6
Renée Kooi <renee@kooi.me>
2019-05-01 11:12:06 +0200

    "polyfill" deepStrictEqual on Node 0.12 and below

--- test/node/index.js
+++ test/node/index.js
@@ -1,6 +1,7 @@
 var spawn = require('child_process').spawn;
 var path = require('path');
 var series = require('run-series');
+var assert = require('assert');
 
 function test(filename) {
   return function(cb) {
@@ -9,6 +10,11 @@ function test(filename) {
       cb(code !== 0 ? new Error('test ' + path.basename(filename) + ' failed') : null);
     });
   };
+}
+
+// "polyfill" deepStrictEqual on Node 0.12 and below
+if (!assert.deepStrictEqual) {
+  assert.deepStrictEqual = assert.strictEqual;
 }
 
 series([
