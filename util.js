6cd2f338dbf174ffc7d8918697bcb127d7fdd301
Himanshu Aggarwal <himanshuapril1@gmail.com>
2020-12-23 23:21:48 +0530

    Added process npm package dependency to fix the ReferenceError

--- util.js
+++ util.js
@@ -19,6 +19,8 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+var process = require('process');
+
 var getOwnPropertyDescriptors = Object.getOwnPropertyDescriptors ||
   function getOwnPropertyDescriptors(obj) {
     var keys = Object.keys(obj);
